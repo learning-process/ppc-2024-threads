70f5aa6d10efda026852596f9580af748fcf9979
FirstStep0 <75475364+FirstStep0@users.noreply.github.com>
2024-04-01 11:27:30 +0300

    seq 7

--- tasks/seq/shokurov_d_convex_hull/func_tests/main.cpp
+++ tasks/seq/shokurov_d_convex_hull/func_tests/main.cpp
@@ -2,15 +2,16 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <random>
 #include <utility>
 #include <vector>
 
-#include "seq/shokurov_d_convex_hull/include/convex_hull.hpp"
+#include "seq/shokurov_d_convex_hull/include/ops_seq.hpp"
 
 TEST(shokurov_d_convex_hull_sequential, Test_one_point) {
   // Create data
   std::vector<pair<double, double>> in;
-  in.push_back({0, 0});
+  in.emplace_back(0, 0);
   std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
@@ -22,7 +23,7 @@ TEST(shokurov_d_convex_hull_sequential, Test_one_point) {
 
   // Create answer
   std::vector<pair<double, double>> ans;
-  ans.push_back({0, 0});
+  ans.emplace_back(0.0, 0.0);
 
   // Create Task
   ConvexHullSequential test(taskDataSeq);
@@ -34,7 +35,7 @@ TEST(shokurov_d_convex_hull_sequential, Test_one_point) {
   size_t k = taskDataSeq->outputs_count[0];
   ASSERT_EQ(ans.size(), k);
 
-  pair<double, double> *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
 
   sort(_out, _out + k);
   sort(ans.begin(), ans.end());
@@ -48,11 +49,11 @@ TEST(shokurov_d_convex_hull_sequential, Test_one_point) {
 TEST(shokurov_d_convex_hull_sequential, Test_many_equals_point) {
   // Create data
   std::vector<pair<double, double>> in;
-  in.push_back({0, 0});
-  in.push_back({0, 0});
-  in.push_back({0, 0});
-  in.push_back({0, 0});
-  in.push_back({0, 0});
+  in.emplace_back(0, 0);
+  in.emplace_back(0, 0);
+  in.emplace_back(0, 0);
+  in.emplace_back(0, 0);
+  in.emplace_back(0, 0);
   std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
@@ -64,7 +65,7 @@ TEST(shokurov_d_convex_hull_sequential, Test_many_equals_point) {
 
   // Create answer
   std::vector<pair<double, double>> ans;
-  ans.push_back({0, 0});
+  ans.emplace_back(0, 0);
 
   // Create Task
   ConvexHullSequential test(taskDataSeq);
@@ -76,10 +77,10 @@ TEST(shokurov_d_convex_hull_sequential, Test_many_equals_point) {
   size_t k = taskDataSeq->outputs_count[0];
   ASSERT_EQ(ans.size(), k);
 
-  pair<double, double> *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
 
-  sort(_out, _out + k);
-  sort(ans.begin(), ans.end());
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
 
   for (size_t i = 0; i < k; ++i) {
     ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
@@ -90,8 +91,8 @@ TEST(shokurov_d_convex_hull_sequential, Test_many_equals_point) {
 TEST(shokurov_d_convex_hull_sequential, Test_two_point) {
   // Create data
   std::vector<pair<double, double>> in;
-  in.push_back({1, 1});
-  in.push_back({0, 0});
+  in.emplace_back(1, 1);
+  in.emplace_back(0, 0);
   std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
@@ -103,8 +104,8 @@ TEST(shokurov_d_convex_hull_sequential, Test_two_point) {
 
   // Create answer
   std::vector<pair<double, double>> ans;
-  ans.push_back({0, 0});
-  ans.push_back({1, 1});
+  ans.emplace_back(0, 0);
+  ans.emplace_back(1, 1);
   // Create Task
   ConvexHullSequential test(taskDataSeq);
   ASSERT_EQ(test.validation(), true);
@@ -115,10 +116,10 @@ TEST(shokurov_d_convex_hull_sequential, Test_two_point) {
   size_t k = taskDataSeq->outputs_count[0];
   ASSERT_EQ(ans.size(), k);
 
-  pair<double, double> *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
 
-  sort(_out, _out + k);
-  sort(ans.begin(), ans.end());
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
 
   for (size_t i = 0; i < k; ++i) {
     ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
@@ -129,15 +130,15 @@ TEST(shokurov_d_convex_hull_sequential, Test_two_point) {
 TEST(shokurov_d_convex_hull_sequential, Test_line_segment) {
   // Create data
   std::vector<pair<double, double>> in;
-  in.push_back({1, 1});
-  in.push_back({0.75, 0.75});
-  in.push_back({0.5, 0.5});
-  in.push_back({0.25, 0.25});
-  in.push_back({0.8, 0.8});
-  in.push_back({0.15, 0.15});
-  in.push_back({0.12, 0.12});
-  in.push_back({0.99, 0.99});
-  in.push_back({0, 0});
+  in.emplace_back(1, 1);
+  in.emplace_back(0.75, 0.75);
+  in.emplace_back(0.5, 0.5);
+  in.emplace_back(0.25, 0.25);
+  in.emplace_back(0.8, 0.8);
+  in.emplace_back(0.15, 0.15);
+  in.emplace_back(0.12, 0.12);
+  in.emplace_back(0.99, 0.99);
+  in.emplace_back(0, 0);
   std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
@@ -149,8 +150,8 @@ TEST(shokurov_d_convex_hull_sequential, Test_line_segment) {
 
   // Create answer
   std::vector<pair<double, double>> ans;
-  ans.push_back({0, 0});
-  ans.push_back({1, 1});
+  ans.emplace_back(0, 0);
+  ans.emplace_back(1, 1);
   // Create Task
   ConvexHullSequential test(taskDataSeq);
   ASSERT_EQ(test.validation(), true);
@@ -161,10 +162,10 @@ TEST(shokurov_d_convex_hull_sequential, Test_line_segment) {
   size_t k = taskDataSeq->outputs_count[0];
   ASSERT_EQ(ans.size(), k);
 
-  pair<double, double> *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
 
-  sort(_out, _out + k);
-  sort(ans.begin(), ans.end());
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
 
   for (size_t i = 0; i < k; ++i) {
     ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
@@ -175,15 +176,15 @@ TEST(shokurov_d_convex_hull_sequential, Test_line_segment) {
 TEST(shokurov_d_convex_hull_sequential, Test_line_segment_2) {
   // Create data
   std::vector<pair<double, double>> in;
-  in.push_back({1, 0});
-  in.push_back({0.75, 0});
-  in.push_back({0.5, 0});
-  in.push_back({0.25, 0});
-  in.push_back({0.8, 0});
-  in.push_back({0.15, 0});
-  in.push_back({0.12, 0});
-  in.push_back({0.99, 0});
-  in.push_back({0, 0});
+  in.emplace_back(1, 0);
+  in.emplace_back(0.75, 0);
+  in.emplace_back(0.5, 0);
+  in.emplace_back(0.25, 0);
+  in.emplace_back(0.8, 0);
+  in.emplace_back(0.15, 0);
+  in.emplace_back(0.12, 0);
+  in.emplace_back(0.99, 0);
+  in.emplace_back(0, 0);
   std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
@@ -195,8 +196,8 @@ TEST(shokurov_d_convex_hull_sequential, Test_line_segment_2) {
 
   // Create answer
   std::vector<pair<double, double>> ans;
-  ans.push_back({0, 0});
-  ans.push_back({1, 0});
+  ans.emplace_back(0, 0);
+  ans.emplace_back(1, 0);
   // Create Task
   ConvexHullSequential test(taskDataSeq);
   ASSERT_EQ(test.validation(), true);
@@ -207,10 +208,10 @@ TEST(shokurov_d_convex_hull_sequential, Test_line_segment_2) {
   size_t k = taskDataSeq->outputs_count[0];
   ASSERT_EQ(ans.size(), k);
 
-  pair<double, double> *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
 
-  sort(_out, _out + k);
-  sort(ans.begin(), ans.end());
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
 
   for (size_t i = 0; i < k; ++i) {
     ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
@@ -221,15 +222,15 @@ TEST(shokurov_d_convex_hull_sequential, Test_line_segment_2) {
 TEST(shokurov_d_convex_hull_sequential, Test_square) {
   // Create data
   std::vector<pair<double, double>> in;
-  in.push_back({0, 0});
-  in.push_back({0, 1});
-  in.push_back({1, 0});
-  in.push_back({1, 1});
-  in.push_back({0.5, 0.5});
-  in.push_back({0.1, 0.6});
-  in.push_back({0.2, 0.2});
-  in.push_back({0.9, 0.9});
-  in.push_back({0.1, 0.1});
+  in.emplace_back(0, 0);
+  in.emplace_back(0, 1);
+  in.emplace_back(1, 0);
+  in.emplace_back(1, 1);
+  in.emplace_back(0.5, 0.5);
+  in.emplace_back(0.1, 0.6);
+  in.emplace_back(0.2, 0.2);
+  in.emplace_back(0.9, 0.9);
+  in.emplace_back(0.1, 0.1);
   std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
@@ -241,10 +242,10 @@ TEST(shokurov_d_convex_hull_sequential, Test_square) {
 
   // Create answer
   std::vector<pair<double, double>> ans;
-  ans.push_back({0, 0});
-  ans.push_back({0, 1});
-  ans.push_back({1, 0});
-  ans.push_back({1, 1});
+  ans.emplace_back(0, 0);
+  ans.emplace_back(0, 1);
+  ans.emplace_back(1, 0);
+  ans.emplace_back(1, 1);
 
   // Create Task
   ConvexHullSequential test(taskDataSeq);
@@ -256,10 +257,10 @@ TEST(shokurov_d_convex_hull_sequential, Test_square) {
   size_t k = taskDataSeq->outputs_count[0];
   ASSERT_EQ(ans.size(), k);
 
-  pair<double, double> *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
 
-  sort(_out, _out + k);
-  sort(ans.begin(), ans.end());
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
 
   for (size_t i = 0; i < k; ++i) {
     ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
@@ -270,13 +271,13 @@ TEST(shokurov_d_convex_hull_sequential, Test_square) {
 TEST(shokurov_d_convex_hull_sequential, Test_triangle) {
   // Create data
   std::vector<pair<double, double>> in;
-  in.push_back({0, 0});
-  in.push_back({1, 0});
-  in.push_back({0, 1});
-  in.push_back({0.1, 0.1});
-  in.push_back({0.2, 0.2});
-  in.push_back({0.5, 0.5});
-  in.push_back({0.1, 0.05});
+  in.emplace_back(0, 0);
+  in.emplace_back(1, 0);
+  in.emplace_back(0, 1);
+  in.emplace_back(0.1, 0.1);
+  in.emplace_back(0.2, 0.2);
+  in.emplace_back(0.5, 0.5);
+  in.emplace_back(0.1, 0.05);
 
   std::vector<pair<double, double>> out(in.size());
 
@@ -289,9 +290,9 @@ TEST(shokurov_d_convex_hull_sequential, Test_triangle) {
 
   // Create answer
   std::vector<pair<double, double>> ans;
-  ans.push_back({0, 0});
-  ans.push_back({1, 0});
-  ans.push_back({0, 1});
+  ans.emplace_back(0, 0);
+  ans.emplace_back(1, 0);
+  ans.emplace_back(0, 1);
 
   // Create Task
   ConvexHullSequential test(taskDataSeq);
@@ -303,10 +304,68 @@ TEST(shokurov_d_convex_hull_sequential, Test_triangle) {
   size_t k = taskDataSeq->outputs_count[0];
   ASSERT_EQ(ans.size(), k);
 
-  pair<double, double> *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
 
-  sort(_out, _out + k);
-  sort(ans.begin(), ans.end());
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
+
+  for (size_t i = 0; i < k; ++i) {
+    ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
+    ASSERT_DOUBLE_EQ(_out[i].second, ans[i].second);
+  }
+}
+
+TEST(shokurov_d_convex_hull_sequential, Test_random) {
+  // Create data
+  std::vector<pair<double, double>> in;
+  std::vector<pair<double, double>> ans;
+  ans.emplace_back(0, 0);
+  ans.emplace_back(1, 0);
+  ans.emplace_back(5, 5);
+  ans.emplace_back(0, 8);
+  ans.emplace_back(-3, 1);
+  size_t co = 5;
+  size_t n = 100;
+  for (size_t i = 0; i < co; ++i) {
+    in.emplace_back(ans[i]);
+  }
+
+  std::mt19937 gen(1.0);
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+
+  for (size_t i = 0; i < n; ++i) {
+    size_t x = i % co;
+    size_t y = (i + 1) % co;
+    double a = dis(gen);
+    pair<double, double> np;
+    np.first = ans[x].first * a + (1.0 - a) * ans[y].first;
+    np.second = ans[x].second * a + (1.0 - a) * ans[y].second;
+    in.emplace_back(np);
+  }
+  std::vector<pair<double, double>> out(in.size());
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  ConvexHullSequential test(taskDataSeq);
+  ASSERT_EQ(test.validation(), true);
+  ASSERT_EQ(test.pre_processing(), true);
+  ASSERT_EQ(test.run(), true);
+  ASSERT_EQ(test.post_processing(), true);
+
+  // Check answer
+  size_t k = taskDataSeq->outputs_count[0];
+  ASSERT_EQ(ans.size(), k);
+
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
 
   for (size_t i = 0; i < k; ++i) {
     ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
--- tasks/seq/shokurov_d_convex_hull/perf_tests/main.cpp
+++ tasks/seq/shokurov_d_convex_hull/perf_tests/main.cpp
@@ -1,17 +1,42 @@
 // Copyright 2024 Shokurov Daniil
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <random>
+#include <utility>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
-#include "seq/example/include/ops_seq.hpp"
+#include "seq/shokurov_d_convex_hull/include/ops_seq.hpp"
 
-TEST(sequential_convexHull_perf_test, test_pipeline_run) {
-  const int count = 100;
-
+TEST(sequential_shokurov_d_convex_hull_perf_test, test_pipeline_run) {
   // Create data
-  std::vector<int> in(1, count);
-  std::vector<int> out(1, 0);
+  std::vector<pair<double, double>> in;
+  std::vector<pair<double, double>> ans;
+  ans.emplace_back(0, 0);
+  ans.emplace_back(1, 0);
+  ans.emplace_back(5, 5);
+  ans.emplace_back(0, 8);
+  ans.emplace_back(-3, 1);
+  size_t co = 5;
+  size_t n = 2000000;
+  for (size_t i = 0; i < co; ++i) {
+    in.emplace_back(ans[i]);
+  }
+
+  std::mt19937 gen(1.0);
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+
+  for (size_t i = 0; i < n; ++i) {
+    size_t x = i % co;
+    size_t y = (i + 1) % co;
+    double a = dis(gen);
+    pair<double, double> np;
+    np.first = ans[x].first * a + (1.0 - a) * ans[y].first;
+    np.second = ans[x].second * a + (1.0 - a) * ans[y].second;
+    in.emplace_back(np);
+  }
+  std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -21,7 +46,7 @@ TEST(sequential_convexHull_perf_test, test_pipeline_run) {
   taskDataSeq->outputs_count.emplace_back(out.size());
 
   // Create Task
-  auto testTaskSequential = std::make_shared<TestTaskSequential>(taskDataSeq);
+  auto test = std::make_shared<ConvexHullSequential>(taskDataSeq);
 
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
@@ -37,18 +62,53 @@ TEST(sequential_convexHull_perf_test, test_pipeline_run) {
   auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(test);
   perfAnalyzer->pipeline_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(count, out[0]);
+
+  // Check answer
+  size_t k = taskDataSeq->outputs_count[0];
+  ASSERT_EQ(ans.size(), k);
+
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
+
+  for (size_t i = 0; i < k; ++i) {
+    ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
+    ASSERT_DOUBLE_EQ(_out[i].second, ans[i].second);
+  }
 }
 
-TEST(sequential_convexHull_perf_test, test_task_run) {
-  const int count = 100;
-
+TEST(sequential_shokurov_d_convex_hull_perf_test, test_task_run) {
   // Create data
-  std::vector<int> in(1, count);
-  std::vector<int> out(1, 0);
+  std::vector<pair<double, double>> in;
+  std::vector<pair<double, double>> ans;
+  ans.emplace_back(0, 0);
+  ans.emplace_back(1, 0);
+  ans.emplace_back(5, 5);
+  ans.emplace_back(0, 8);
+  ans.emplace_back(-3, 1);
+  size_t co = 5;
+  size_t n = 2000000;
+  for (size_t i = 0; i < co; ++i) {
+    in.emplace_back(ans[i]);
+  }
+
+  std::mt19937 gen(1.0);
+  std::uniform_real_distribution<> dis(0.0, 1.0);
+
+  for (size_t i = 0; i < n; ++i) {
+    size_t x = i % co;
+    size_t y = (i + 1) % co;
+    double a = dis(gen);
+    pair<double, double> np;
+    np.first = ans[x].first * a + (1.0 - a) * ans[y].first;
+    np.second = ans[x].second * a + (1.0 - a) * ans[y].second;
+    in.emplace_back(np);
+  }
+  std::vector<pair<double, double>> out(in.size());
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -58,7 +118,7 @@ TEST(sequential_convexHull_perf_test, test_task_run) {
   taskDataSeq->outputs_count.emplace_back(out.size());
 
   // Create Task
-  auto testTaskSequential = std::make_shared<TestTaskSequential>(taskDataSeq);
+  auto test = std::make_shared<ConvexHullSequential>(taskDataSeq);
 
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
@@ -74,8 +134,21 @@ TEST(sequential_convexHull_perf_test, test_task_run) {
   auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(test);
   perfAnalyzer->task_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(count, out[0]);
+
+  // Check answer
+  size_t k = taskDataSeq->outputs_count[0];
+  ASSERT_EQ(ans.size(), k);
+
+  auto *_out = reinterpret_cast<pair<double, double> *>(taskDataSeq->outputs[0]);
+
+  std::sort(_out, _out + k);
+  std::sort(ans.begin(), ans.end());
+
+  for (size_t i = 0; i < k; ++i) {
+    ASSERT_DOUBLE_EQ(_out[i].first, ans[i].first);
+    ASSERT_DOUBLE_EQ(_out[i].second, ans[i].second);
+  }
 }
