f1027af824d444e1fe2c09ebc60c7b8f555bd697
STELLSAN <stellsandio@gmail.com>
2024-04-07 23:41:42 +0300

    fix_5

--- tasks/seq/nedelin_d_cannons_algorithm/func_tests/main.cpp
+++ tasks/seq/nedelin_d_cannons_algorithm/func_tests/main.cpp
@@ -170,3 +170,36 @@ TEST(Nedelin_d_block_cannons_seq, Multiplication_100x100) {
     ASSERT_EQ(res[i], out[i]);
   }
 }
+
+TEST(Nedelin_d_block_cannons_seq, Multiplication_250x250) {
+  int n = 250;
+  int m = 250;
+
+  std::vector<double> in_mtrx_A = getRandomMatrix(n, m);
+  std::vector<double> in_mtrx_B = getRandomMatrix(n, m);
+  std::vector<double> out(n * m);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_mtrx_A.data()));
+  taskDataSeq->inputs_count.emplace_back(in_mtrx_A.size());
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_mtrx_B.data()));
+  taskDataSeq->inputs_count.emplace_back(in_mtrx_B.size());
+
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&m));
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  std::vector<double> res = multiplyMatrix(in_mtrx_A, in_mtrx_B, n, m);
+
+  TestTaskSequentialNedelinCannon testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+
+  for (size_t i = 0; i < res.size(); ++i) {
+    ASSERT_EQ(res[i], out[i]);
+  }
+}
