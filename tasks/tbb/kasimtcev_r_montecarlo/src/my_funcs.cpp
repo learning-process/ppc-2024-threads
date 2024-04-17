91ddddd16491c354673c7b1a801345fbb74167bb
Касимцев Роман <Romasias1@outlook.com>
2024-04-17 23:37:25 +0300

    trird commit_3

--- tasks/tbb/kasimtcev_r_montecarlo/src/my_funcs.cpp
+++ tasks/tbb/kasimtcev_r_montecarlo/src/my_funcs.cpp
@@ -2,8 +2,6 @@
 
 #include <cmath>
 
-#include "tbb/kasimtcev_r_montecarlo/include/my_funcs.hpp"
-
 double flinear(double x, double y) { return x + y; }
 double fsinxsiny(double x, double y) { return sin(x) + sin(y); }
 double fcosxcosy(double x, double y) { return cos(x) + cos(y); }
