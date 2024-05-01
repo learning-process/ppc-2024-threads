7627fd2b472a6308dd9dfff9e2d8f6e7e8978df2
Roboric-sun <cfif100808@icloud.com>
2024-05-01 23:56:38 +0300

    3

--- tasks/seq/shipitsin_a_rect_integral/func_tests/main.cpp
+++ tasks/seq/shipitsin_a_rect_integral/func_tests/main.cpp
@@ -10,7 +10,7 @@ TEST(shipitsin_a_rect_integral_seq, Test_x_sub_y_1) {
   Func f = shipitsin_flin;
   uint64_t n = 100;
 
-  std::vector<double> in = {0, 4, 0, 4};
+  std::vector<double> in = {0, 2, 0, 2};
   std::vector<double> out(1, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -37,7 +37,7 @@ TEST(shipitsin_a_rect_integral_seq, Test_x_sub_y_2) {
   Func f = shipitsin_flin;
   uint64_t n = 100;
 
-  std::vector<double> in = {-5, 3, 4, 9};
+  std::vector<double> in = {-5, 1, 4, 9};
   std::vector<double> out(1, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -64,7 +64,7 @@ TEST(shipitsin_a_rect_integral_seq, Test_x_mul_y) {
   Func f = shipitsin_fxy;
   uint64_t n = 100;
 
-  std::vector<double> in = {0, 3, 1, 4};
+  std::vector<double> in = {0, 2, 1, 2};
   std::vector<double> out(1, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -91,7 +91,7 @@ TEST(shipitsin_a_rect_integral_seq, Test_y_sin_x) {
   Func f = shipitsin_fysinx;
   uint64_t n = 100;
 
-  std::vector<double> in = {-2, 4, 1, 4};
+  std::vector<double> in = {-2, 2, 1, 2};
   std::vector<double> out(1, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -118,7 +118,7 @@ TEST(shipitsin_a_rect_integral_seq, Test_x_exp_y) {
   Func f = shipitsin_fxexpy;
   uint64_t n = 1000;
 
-  std::vector<double> in = {0, 4, 1, 4};
+  std::vector<double> in = {0, 5, 1, 2};
   std::vector<double> out(1, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
