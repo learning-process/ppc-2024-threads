fe88eed9f034782d1fe89a04eeb730fa3efde4bd
PavelLSmirnov <pavel1201s@yandex.ru>
2024-05-17 21:06:55 +0300

    feat lab 1

---
+++ tasks/seq/smirnov_p_sort_double_batcher/include/ops_seq.hpp
@@ -0,0 +1,27 @@
+// Copyright Smirnov Pavel 2024
+#pragma once
+
+#include <algorithm>
+#include <random>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+class SortDoubleBatcherSequential : public ppc::core::Task {
+ public:
+  explicit SortDoubleBatcherSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<double> arr;
+  std::vector<double> res;
+};
+
+std::vector<double> BatchersMerge(std::vector<std::vector<double>>& subvectors);
+void PartSort(std::vector<std::vector<double>>& parts, std::vector<double>& side);
+std::vector<double> BitwiseSortBatcher(std::vector<double> v);
+std::vector<double> RandomVector(int sizeVec, double minValue, double maxValue);
